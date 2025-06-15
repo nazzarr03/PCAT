8b5f889226349bb55af3e9c5a7113349a3fe093a
nazzarr03 <nazararik585@gmail.com>
2025-06-15 19:04:45 +0300

    static files uploaded

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
-const fn = function () {
-  console.log('function working');
-};
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+/* const myLogger = (req, res, next) => {
+  console.log('middleware log');
+  next(); // bir sonraki middleware geçmeyi sağlar. get isteği de bir middleware sayılır. 
+  // req ile res arasında olan her şey middleware sayılır.
+}; */
+
+// middlewares
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+});
+
+const port = 3003;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
